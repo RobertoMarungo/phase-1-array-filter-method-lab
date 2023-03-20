28f8f93b1922ef0e490b42bf52c947a7744d6908
Roberto Marungo <100833599+RobertoMarungo@users.noreply.github.com>
2023-03-20 23:09:53 +0000

    Completed assignmetn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(testArr, testString) {
+  return testArr.filter(
+    (result) => result.toLowerCase() === testString.toLowerCase()
+  );
+}
+
+function fuzzyMatch(testArr, testString) {
+  return testArr.filter(
+    (result) => result.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(testArr, testString) {
+  return testArr.filter((result) => result.name === testString);
+}
